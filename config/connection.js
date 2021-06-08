40471b2f74ce228a0b162cff14961d6886e86861
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-08 09:56:37 -0400

    Added connection info to /config/connection.js

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'TjsS#ycyth_T,L3b36g0',
+  database: 'schema.sql',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  connection.end();
+});
