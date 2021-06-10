885d5aba7e43e4e8605d5da9ed351ffd78cc5993
Roy Sizemore <rsizemore@swampcastle.home>
2021-06-10 15:58:45 -0400

    Added functions to model/index.js. Added menu function to /index.js

--- index.js
+++ index.js
@@ -1,66 +1,72 @@
 const inquirer = require('inquirer');
 const Org = require('./model');
 
-inquirer
-    .prompt([
-        {
-            name: 'userInput',
-            message: 'Please select from the following: ',
-            choices: [
-                'Display all employees',
-                'Display all employees by department',
-                'Add an employee',
-                'Delete an employee',
-                "Update an employee's role",
-                "Update an employee's manager",
-                'Show all roles',
-                'Create new role',
-                'Delete role',
-                'Show all departments',
-                'Add a department',
-                'Delete a department',
-                'Exit'
-            ]
-        }
+const menu = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'userInput',
+                message: 'Please select from the following: ',
+                choices: [
+                    'Display all employees',
+                    'Display all employees by department',
+                    'Add an employee',
+                    'Delete an employee',
+                    "Update an employee's role",
+                    "Update an employee's manager",
+                    'Show all roles',
+                    'Create new role',
+                    'Delete role',
+                    'Show all departments',
+                    'Add a department',
+                    'Delete a department',
+                    'Exit'
+                ]
+            }
     ]).then((answer) => {
         switch(answer.userInput) {
             case 'Display all employees':
-                return selectAllEmployees();
+                return showAllEmployees();
 
             case 'Display all employees by department':
-                return selectAllEmployeesByDepartmentId(departmentId);
+                return selectAllEmployeesByDepartmentId();
             
             case 'Add an employee':
-                return createEmployee(employee);
+                return createEmployee();
             
             case 'Delete an employee':
-                return deleteEmployeeById(employeeId);
+                return deleteEmployeeById();
             
             case "Update an employee's role":
-                return updateEmployeeRoleById(employeeId, roleId);
+                return updateEmployeeRoleById();
             
             case "Update an employee's manager":
-                return updateEmployeeManagerByIds(employeeId, managerId);
+                return updateEmployeeManagerByIds();
 
             case 'Show all roles':
                 return selectAllRoles();
 
             case 'Create new role':
-                return createRole(role);
+                return createRole();
 
             case 'Delete role':
-                return deleteRoleById(roleId);
+                return deleteRoleById();
 
             case 'Show all departments':
                 return selectAllDepartments();
 
             case 'Add a department':
-                return createDepartment(department);
+                return createDepartment();
 
             case 'Delete a department':
-                return deleteDepartmentById(departmentId);
+                return deleteDepartmentById();
 
             default:
                 process.exit();
         };
     });
+};
+
+menu();
+
+module.exports = menu;
--- model/index.js
+++ model/index.js
@@ -1,4 +1,8 @@
-const connection = require("../config/connection");
+const inquirer = require("inquirer");
+const connection = require('../config/connection');
+const db = require('../db');
+const menu = require('../index');
+require('console.table');
 
 class Org {
     constructor(connection) {
@@ -8,64 +12,291 @@ class Org {
     // Employee query functions
     // Create a new employee
     createEmployee(employee) {
-        return this.connection.query("insert into employees set ?", employee);
+        const employee = inquirer.prompt([
+            {
+              name: 'first_name',
+              message: "Please enter the employee's first name: ",
+              type: 'input'
+            },
+            {
+              name: 'last_name',
+              message: "Please enter the employee's last name",
+              type: 'input'
+            }
+          ]);
+        
+          const role = selectAllRoles().map(({ id, title }) => ({
+            name: title,
+            value: id
+          }));
+        
+          const { roleId } = inquirer.prompt({
+            name: 'roleId',
+            message: "Please enter the employee's role: ",
+            type: 'list',
+            choices: role
+          });
+        
+          employee.role_id = roleId;
+        
+          const manager = employees.map(({ id, first_name, last_name }) => ({
+            name: `${first_name} ${last_name}`,
+            value: id
+          }));
+          manager.unshift({ name: 'None', value: null });
+        
+          const { managerId } = inquirer.prompt({
+            name: 'managerId',
+            message: "Please enter the employee's manager: ",
+            type: 'list',
+            choices: manager
+          });
+        
+          employee.manager_id = managerId;
+        
+        console.table(this.connection.query("insert into employees set ?", employee));
+        menu();
     };
 
     // Selects all employees and joins with roles/departments tables
+    showAllEmployees() {
+        console.table(this.connection.query("select employees.id, employees.first_name, employees.last_name, role.title, department.name as department, role.salary, concat(manager.first_name, ' ', manager.last_name) as manager from employees left join role on employees.role_id = role.id left join department on role.department_id = department.id left join employees manager on manager.id = employees.manager_id;"));
+        menu();
+    };
+
+    // Just returns all employees w/o displaying them
     selectAllEmployees() {
         return this.connection.query("select employees.id, employees.first_name, employees.last_name, role.title, department.name as department, role.salary, concat(manager.first_name, ' ', manager.last_name) as manager from employees left join role on employees.role_id = role.id left join department on role.department_id = department.id left join employees manager on manager.id = employees.manager_id;");
     };
 
     // Updates employee's role
     updateEmployeeRoleById(employeeId, roleId) {
-        return this.connection.query("update employees set role_id = ? where id = ?", [roleId, employeeId]);
+        const allEmployees = selectAllEmployees();
+
+        const employee = allEmployees.map(({ id, first_name, last_name }) => ({
+          name: `${first_name} ${last_name}`,
+          value: id
+        }));
+      
+        const { employeeId } = inquirer.prompt([
+          {
+            name: 'employeeId',
+            message: 'Please enter an employee to update their role: ',
+            type: 'list',
+            choices: employee
+          }
+        ]);
+      
+        const allRoles = selectAllRoles();
+      
+        const role = allRoles.map(({ id, title }) => ({
+          name: title,
+          value: id
+        }));
+      
+        const { roleId } = inquirer.prompt([
+          {
+            name: 'roleId',
+            message: 'What is the new role?',
+            type: 'list',
+            choices: role
+          }
+        ]);
+      
+        console.table(this.connection.query("update employees set role_id = ? where id = ?", [roleId, employeeId]));
+        menu();
     };
     
     // Updates employee's manager by employee ID and manager ID
     updateEmployeeManagerByIds(employeeId, managerId) {
-        return this.connection.query("update employees set manager_id = ? where id = ?", [managerId, employeeId]);
+        const allEmployees = selectAllEmployees();
+
+        const employee = allEmployees.map(({ id, first_name, last_name }) => ({
+          name: `${first_name} ${last_name}`,
+          value: id
+        }));
+      
+        const { employeeId } = inquirer.prompt([
+          {
+            name: 'employeeId',
+            message: 'Please enter an employee to update: ',
+            type: 'list',
+            choices: employee
+          }
+        ]);
+      
+        const allManagers = this.connection.query("select id, first_name, last_name from employee where id != ?", employeeId);
+      
+        const manager = allManagers.map(({ id, first_name, last_name }) => ({
+          name: `${first_name} ${last_name}`,
+          value: id
+        }));
+      
+        const { managerId } = inquirer.prompt([
+          {
+            name: 'managerId',
+            message: 'Please select a manager for this employee: ',
+            type: 'list',
+            choices: manager
+          }
+        ]);
+      
+        console.table(this.connection.query("update employees set manager_id = ? where id = ?", [managerId, employeeId]));
+        menu();
     };
 
     // Delete's an employee by their ID
     deleteEmployeeById(employeeId) {
-        return this.connection.query("delete from employees where id = ?", employeeId);
+        const allEmployees = selectAllEmployees();
+
+        const employee = allEmployees.map(({ id, first_name, last_name }) => ({
+          name: `${first_name} ${last_name}`,
+          value: id
+        }));
+      
+        const { employeeId } = inquirer.prompt([
+          {
+            name: 'employeeId',
+            message: 'Please select an employee to remove: ',
+            type: 'list',
+            choices: employee
+          }
+        ]);
+      
+        console.table(this.connection.query("delete from employees where id = ?", employeeId));
+        menu();
     };
 
     // Role query functions
     // Creates a new role
     createRole(role) {
-        return this.connection.query("insert into role set ?", role);
+        const allDepartments = selectAllDepartments();
+
+        const department = allDepartments.map(({ id, name }) => ({
+          name: name,
+          value: id
+        }));
+      
+        const role = inquirer.prompt([
+          {
+            name: 'title',
+            message: "Please enter the role's name: ",
+            type: 'input'
+          },
+          {
+            name: 'salary',
+            message: "Please enter the role's salary: ",
+            type: 'input'
+          },
+          {
+            name: 'department_id',
+            message: "Please enter the role's department: ",
+            type: 'list',
+            choices: department
+          }
+        ]);
+      
+        console.table(this.connection.query("insert into role set ?", role));
+        menu();
     };
 
     // Selects all roles and joins w/departments DB
+    showAllRoles() {
+        console.table(this.connection.query("select role.id, role.title, department.name as department, role.salary from role left join department on role.department_id = department.id;"));
+        menu();
+    };
+
+    // Just returns all roles w/o displaying them
     selectAllRoles() {
-        return this.connection.query("select role.id, role.title, department.name as department, role.salary from role left join department on role.department_id = department.id;");
+        return this.connection.query("select role.id, role.title, department.name as department, role.salary from role left join department on role.department_id = department.id;")
     };
 
     // Deletes a role by ID
     deleteRoleById(roleId) {
-        return this.connection.query("delete from role where id = ?", roleId);
+        const allRoles = selectAllRoles();
+
+        const role = allRoles.map(({ id, title }) => ({
+          name: title,
+          value: id
+        }));
+      
+        const { roleId } = await prompt([
+          {
+            name: 'roleId',
+            message: 'Please select a role to remove: ',
+            type: 'list',
+            choices: role
+          }
+        ]);
+      
+        console.table(this.connection.query("delete from role where id = ?", roleId));
+        menu();
     };
 
     // Department query functions
     // Selects all departments and joins w/employees and role DBs (includes budget bonus)
+    showAllDepartments() {
+        console.table(this.connection.query("select department.id, department.name, sum(role.salary) as utilized_budget from department left join role on role.department_id = department.id left join employee on employee.role_id = role.id group by department.id, department.name"));
+        menu();
+    };
+
+    // Just returns all departments w/o displaying them
     selectAllDepartments() {
         return this.connection.query("select department.id, department.name, sum(role.salary) as utilized_budget from department left join role on role.department_id = department.id left join employee on employee.role_id = role.id group by department.id, department.name");
     };
 
     // Creates a department
     createDepartment(department) {
-        return this.connection.query("insert into department set ?", department);
+        const department = inquirer.prompt([
+            {
+              name: 'name',
+              message: 'Please enter a name for the new department: ',
+              type: 'input'
+            }
+          ]);
+        
+        console.table(this.connection.query("insert into department set ?", department));
+        menu();
     };
 
     // Deletes a department
     deleteDepartmentById(departmentId) {
-        return this.connection.query("delete from department where id = ?", departmentId);
+        const allDepartments = selectAllDepartments();
+
+        const department = allDepartments.map(({ id, name }) => ({
+          name: name,
+          value: id
+        }));
+      
+        const { departmentId } = await prompt({
+          name: 'departmentId',
+          message: 'Please select a department to remove: ',
+          type: 'list',
+          choices: department
+        });
+      
+        console.table(this.connection.query("delete from department where id = ?", departmentId));
+
+        menu();
     };
 
     // Select all employees by department ID and joins with role DB
     selectAllEmployeesByDepartmentId(departmentId) {
-        return this.connection.query("select employees.id, employees.first_name, employees.last_name, role.title from employees left join role on employees.role_id = role.id left join department department on role.department_id = department.id where department.id = ?;", departmentId);
+        const department = selectAllDepartments().map(({ id, name }) => ({
+          name: name,
+          value: id
+        }));
+      
+        const { departmentId } = inquirer.prompt([
+          {
+            type: 'list',
+            name: 'departmentId',
+            message: 'Please select a department: ',
+            choices: department
+          }
+        ]);
+        console.table(this.connection.query("select employees.id, employees.first_name, employees.last_name, role.title from employees left join role on employees.role_id = role.id left join department department on role.department_id = department.id where department.id = ?;", departmentId));
+        menu();
     };
 };
 
